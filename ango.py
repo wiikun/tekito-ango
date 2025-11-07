b277d26c360c163b503e7c002ca9311dc47df064
wiikun <qwop90175@gmail.com>
2025-11-07 19:00:21 +0900

    update

--- ango.py
+++ ango.py
@@ -6,8 +6,23 @@ with open(sys.argv[1],"rb") as file:
 lst = bytearray()
 old = 0
 
+if len(sys.argv) < 4:
+    print("key not found")
+    sys.exit(1)
+
+key = 0
+kold = 0
+
+kbyte = sys.argv[3].encode("utf-8")
+
+for i in kbyte:
+    key += (i + kold)
+    kold = i
+
+key %= 256
+
 for i in raw:
-    lst.append((i + old) % 256)
+    lst.append((i + old + key) % 256)
     old = i % 256
 
 with open(sys.argv[2],"wb") as file:
--- hukugo.py
+++ hukugo.py
@@ -6,8 +6,23 @@ with open(sys.argv[1],"rb") as file:
 lst = bytearray()
 old = 0
 
+if len(sys.argv) < 4:
+    print("key not found")
+    sys.exit(1)
+
+key = 0
+kold = 0
+
+kbyte = sys.argv[3].encode("utf-8")
+
+for i in kbyte:
+    key += (i + kold)
+    kold = i
+
+key %= 256
+
 for i in raw:
-    dec = (i - old) % 256
+    dec = (i - old - key) % 256
     lst.append(dec)
     old = dec
 
