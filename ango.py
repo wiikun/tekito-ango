3a9553f20fa07647aa04ed24c82686a20532c05e
wiikun <qwop90175@gmail.com>
2025-11-08 10:42:30 +0900

    Add files via upload

--- ango.py
+++ ango.py
@@ -1,5 +1,6 @@
 import sys
 import getpass
+import base64
 
 with open(sys.argv[1],"rb") as file:
     raw = file.read()
@@ -12,12 +13,16 @@ key = bytearray(getpass.getpass(prompt="input key:").encode("utf-8"))
 klen = len(key)
 
 keyi = 0
+loter2 = 0
+loter3 = 0
 
 for i in raw:
     lst.append((i + old + key[keyi]) % 256)
-    old = i
-    key[keyi] = abs(key[keyi] + ((key[keyi] % 5) + 1) * (-1) if key[keyi] % 5 % 2 == 0 else (1)) % 256
+    key[keyi] = abs(key[keyi] + (old + (key[keyi] % 5) + 1 + loter2 - loter3) * (-1) if key[keyi] % 5 % 2 == 0 else (1)) % 256
     keyi = keyi + 1 if keyi + 1 < klen else 0
+    old = i
+    loter2 = i % 4
+    loter3 = i % 5
 
 with open(sys.argv[2],"wb") as file:
-    file.write(lst)
+    file.write(base64.b64encode(lst))
--- hukugo.py
+++ hukugo.py
@@ -1,8 +1,9 @@
 import sys
 import getpass
+import base64
 
-with open(sys.argv[1],"rb") as file:
-    raw = file.read()
+with open(sys.argv[1],"r") as file:
+    raw = base64.b64decode(file.read())
 
 lst = bytearray()
 old = 0
@@ -16,13 +17,17 @@ key = bytearray(getpass.getpass(prompt="input key:").encode("utf-8"))
 klen = len(key)
 
 keyi = 0
+loter2 = 0
+loter3 = 0
 
 for i in raw:
     dec = (i - old - key[keyi]) % 256
     lst.append(dec)
-    old = dec
-    key[keyi] = abs(key[keyi] + ((key[keyi] % 5) + 1) * (-1) if key[keyi] % 5 % 2 == 0 else (1)) % 256
+    key[keyi] = abs(key[keyi] + (old + (key[keyi] % 5) + 1 + loter2 - loter3) * (-1) if key[keyi] % 5 % 2 == 0 else (1)) % 256
     keyi = keyi + 1 if keyi + 1 < klen else 0
+    old = dec
+    loter2 = dec % 4
+    loter3 = dec % 5
 
 with open(sys.argv[2],"wb") as file:
     file.write(lst)
